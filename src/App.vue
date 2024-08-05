fca47e43c2e7e005333b2daf1cbb0a885c6c48a2
sol0veyy <solovevdaniil03@mail.ru>
2024-08-05 11:52:22 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+  import { RouterView } from 'vue-router'
+  import Header from './components/Header/HeaderApp.vue';
+</script>
+
+<template>
+  <Header />
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+  import MainSection from '../components/Sections/MainSection/MainSection.vue';
+</script>
+
+<template>
+  <main>
+    <MainSection />
+  </main>
+</template>
